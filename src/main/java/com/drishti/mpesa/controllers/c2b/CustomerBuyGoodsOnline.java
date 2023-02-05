e1729c70e6e3ee657258cccd62122c7958e5d9ef
Vivek Singh <40670506+vivekpegasusone@users.noreply.github.com>
2023-02-05 14:21:10 +0530

    Update CustomerBuyGoodsOnline.java
    Updating mapping from Get to Post

--- src/main/java/com/drishti/mpesa/controllers/c2b/CustomerBuyGoodsOnline.java
+++ src/main/java/com/drishti/mpesa/controllers/c2b/CustomerBuyGoodsOnline.java
@@ -1,10 +1,8 @@
 package com.drishti.mpesa.controllers.c2b;
 
-import com.drishti.mpesa.model.c2b.ConfirmationMessage;
 import com.drishti.mpesa.model.c2b.ValidationMessage;
 import com.drishti.mpesa.model.c2b.ValidationResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +11,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("c2b")
 public class CustomerBuyGoodsOnline {
 
-    @GetMapping(path = "validate", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
+    @PostMapping(path = "validate", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    //@ResponseBody
     public ValidationResponse validate(@RequestBody ValidationMessage message){
        /*
        * If External Validation is disabled this method will not be called.
@@ -24,7 +22,7 @@ public class CustomerBuyGoodsOnline {
         return response;
     }
 
-    @GetMapping(path = "confirmation", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "confirmation", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
     public void confirmation(@RequestBody ValidationMessage message) throws JsonProcessingException {
         System.out.println("***************************");
