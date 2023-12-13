e3a97a65269d4aafebf2068f4ab7374302512bda
Vivek Singh <toviveik@gmail.com>
2023-12-13 11:52:21 +0530

    Code reformat.

--- src/main/java/com/drishti/mpesa/controllers/c2b/CustomerBuyGoodsOnline.java
+++ src/main/java/com/drishti/mpesa/controllers/c2b/CustomerBuyGoodsOnline.java
@@ -3,7 +3,6 @@ package com.drishti.mpesa.controllers.c2b;
 import com.drishti.mpesa.model.c2b.ValidationMessage;
 import com.drishti.mpesa.model.c2b.ValidationResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +13,11 @@ public class CustomerBuyGoodsOnline {
 
     @GetMapping(path = "validate", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     //@ResponseBody
-    public ValidationResponse validate(@RequestBody ValidationMessage message){
-       /*
-       * If External Validation is disabled this method will not be called.
-       * By default, it is disabled.
-       */
+    public ValidationResponse validate(@RequestBody ValidationMessage message) {
+        /*
+         * If External Validation is disabled this method will not be called.
+         * By default, it is disabled.
+         */
         ValidationResponse response = new ValidationResponse();
         return response;
     }
